e38a14cfa5a2376c55bdaa82bcf95d7635a6a794
Joana Niermann <joana.niermann@cern.ch>
2020-11-04 19:57:28 +0100

    Fixed logging and added a reduced set of cmd line arguments

--- Examples/Run/DD4hep/DD4hepDetectorGdmlExample.cpp
+++ Examples/Run/DD4hep/DD4hepDetectorGdmlExample.cpp
@@ -13,18 +13,26 @@
 #include "Acts/Utilities/Logger.hpp"
 
 #include <boost/program_options.hpp>
-#include <fstream>
-#include <memory>
 
 #include <DD4hep/Detector.h>
 
 using namespace ActsExamples;
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[]) {
   // Setup and parse options
   auto desc = Options::makeDefaultOptions();
-  Options::addOutputOptions(desc);
-  Options::addDD4hepOptions(desc);
+  desc.add_options()
+      ("dd4hep-input",
+      po::value<std::vector<std::string>>()->multitoken()->default_value(
+          {"file:Detectors/DD4hepDetector/compact/OpenDataDetector/"
+           "OpenDataDetector.xml"}),
+      "The locations of the input DD4hep files, use 'file:foo.xml'. In case "
+      "you want to read in multiple files, just seperate the strings by "
+      "space.")
+    ("output-dir", po::value<std::string>()->default_value(""),
+                   "Output directory location.");
   auto vm = Options::parse(desc, argc, argv);
   if (vm.empty()) {
     return EXIT_FAILURE;
@@ -33,43 +41,33 @@ int main(int argc, char* argv[]) {
   // Setup the DD4hep detector
   dd4hep::Detector& dd4hepDet = dd4hep::Detector::getInstance();
 
-  // DD4hep detector xml inputfile
-  //std::string inputFile = vm["dd4hep-input"].as<read_strings>();
-  std::string inputFile = "/afs/cern.ch/user/j/jonierma/Project_Files/src/ACTS/Examples/Detectors/DD4hepDetector/compact/OpenDataDetector/OpenDataDetector.xml";
-  const char* inputFiles[] = {inputFile.c_str()};
+  // DD4hep detector xml inputfiles
+  std::vector<std::string> geoFiles = vm["dd4hep-input"].as<std::vector<std::string>>();
+  const char* inputFiles[] = {geoFiles[0].c_str()};
 
   // Output directory
-  //std::string outputDir = vm["output-dir"].template as<std::string>();
-  //std::string outputFile = "/eos/user/j/jonierma/data/geometry/OpenDataDetector/DD4hep_detector.gdml";
-  std::string outputFile = "dd4hep_detector.gdml";
+  std::string outputDir = vm["output-dir"].template as<std::string>();
+  std::string outputFile = joinPaths(outputDir,"dd4hep_detector.gdml");
+  // Due to dd4hep argument parsing, an output file name cannot be at 0th position
   const char* outputFiles[] = {0,outputFile.c_str()};
   
-  // Logfile
-  std::ofstream logfile("log.txt");
-  std::unique_ptr<const Acts::Logger> myLogger
-      = Acts::getDefaultLogger("MyLogger", Acts::Logging::INFO, &logfile);
-  //ACTS_LOCAL_LOGGER(myLogger);
+  // Logging
+  auto logLevel = ActsExamples::Options::readLogLevel(vm);
+  ACTS_LOCAL_LOGGER(Acts::getDefaultLogger("DD4hep2Gdml", logLevel));
+
   // Call dd4hep GDML plugin to convert the geomtery and save it to disk
   // see dd4hep/UtilityApps/src/run_plugin.h
   try {
     dd4hepDet.apply("DD4hep_CompactLoader",1,(char**)inputFiles);
     dd4hepDet.apply("DD4hepGeometry2GDML", 1, (char**)&outputFiles[1]);
-    //ACTS_INFO("Completed conversion");
-    logfile.close();
+    ACTS_INFO("Completed conversion");
     return EXIT_SUCCESS;
   }
   catch(const std::exception& e)  {
-    //dd4hep::except("DD4hep: RunPlugin","++ Exception while executing plugin "
-    //               "%s:\n\t\t%s", name ? name : "<unknown>", e.what());
-    //ACTS_INFO("DD4hep plugin error: " + std::string(e.what()));
-    std::cout << "DD4hep exception: " << std::string(e.what()) << std::endl;
+    ACTS_FATAL("DD4hep plugin error: " + std::string(e.what()));
   }
   catch(...)  {
-    //dd4hep::except("DD4hep: RunPlugin","++ UNKNOWN Exception while executing "
-    //               "plugin %s.",name ? name : "<unknown>");
-    //ACTS_INFO("DD4hep plugin error: <unknown>");
-    std::cout << "DD4hep exception: <unknown>" << std::endl;
+    ACTS_FATAL("DD4hep plugin error: <unknown>");
   }
-  logfile.close();
   return EXIT_FAILURE;
 }
